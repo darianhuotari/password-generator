9d2cfe3051bf31ad124546891ee8bddfa340a778
Darian Huotari <darhuotari1@gmail.com>
2024-09-06 10:47:33 -0500

    remove pwdName, simplify modules

--- app/PasswordGenerator/pwdgen.py
+++ app/PasswordGenerator/pwdgen.py
@@ -1,47 +1,39 @@
 import os
 import string, secrets
 
-class passwordGenerator:
+def generate_password(pwdLength: int):
 
-    def __init__(self):
-        pass
 
-    @staticmethod
+    # Define a default password length # TODO - check if value is empty or string?
+    defaultLength =int(20)
 
-    def generate_password(pwdName: str, pwdLength: int):
-
+    # Raise an exception instead of return
+    if pwdLength == 0:
+        return ("Password length cannot be 0")
+    elif pwdLength < 0:
+        return ("Password length cannot be negative")
     
-        # Define a default password length # TODO - check if value is empty or string?
-        defaultLength =int(20)
+    # Build lists of candidate characters to be used in the password
+    s1 = string.ascii_letters
+    s2 = string.digits
+    s3 = """ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~""" # OWASP special characters: https://owasp.org/www-community/password-special-characters
 
-        if pwdLength == 0:
-            return ("Password length cannot be 0")
-        elif pwdLength < 0:
-            return ("Password length cannot be negative")
-        elif pwdLength < 12:
-            return("Minimum password length is 12. Please try again.")
-        
-        # Build lists of candidate characters to be used in the password
-        s1 = string.ascii_letters
-        s2 = string.digits
-        s3 = """ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~""" # OWASP special characters: https://owasp.org/www-community/password-special-characters
+    # Create a list of characters to be used in the password
+    characterList = s1 + s2 + s3
 
-        # Create a list of characters to be used in the password
-        characterList = s1 + s2 + s3
+    while True: # We could get rid of the while loop by building a list and then randomizing it
 
-        while True:
+        # Concatenate random characters from the characterList using the secrets module, up to the pwdLength
+        # The password should have at least 2 uppercase, lowercase, numbers, and special characters
+        password = ''.join(secrets.choice(characterList) for i in range(pwdLength))
+        if (sum(char.islower() for char in password) >=2
+                and sum(char.isupper() for char in password) >=2
+                and sum(char.isdigit() for char in password) >=2
+                and sum(char.isalnum() for char in password) >=2):
+            break
 
-            # Concatenate random characters from the characterList using the secrets module, up to the pwdLength
-            # The password should have at least 2 uppercase, lowercase, numbers, and special characters
-            password = ''.join(secrets.choice(characterList) for i in range(pwdLength))
-            if (sum(char.islower() for char in password) >=2
-                    and sum(char.isupper() for char in password) >=2
-                    and sum(char.isdigit() for char in password) >=2
-                    and sum(char.isalnum() for char in password) >=2):
-                break
-
-        print ("Successfully generated password with a length of", pwdLength, "characters!")        
-        return (pwdName, password)
+    print ("Successfully generated password with a length of", pwdLength, "characters!")        
+    return (password)
 
 
 
--- app/main.py
+++ app/main.py
@@ -10,21 +10,25 @@ async def root():
 
 
 @app.get("/password", summary="Generate a random password", tags=['Password Generator'])
-async def read_item(pass_length: Optional[int] = "20",
-                    pass_name: Optional[str] = "Password"):
+async def read_item(pass_length: Optional[int] = "20"):
     """
     Parameters:<br>
     - pass_length: (int) Length of password to be generated.
-    - pass_name: (str) Name of password to be generated.
 
     Returns:<br>
-    - name of randomly generated password
     - randomly generated password
 
     """
+    if pass_length is None:
+        return ("This should not happen")
+    if pass_length == 0:
+        return ("Password length cannot be 0")
+    elif pass_length < 0:
+        return ("Password length cannot be negative")
+    elif pass_length < 12:
+        return("Minimum password length is 12. Please try again.")
 
-    generator = pwdgen.passwordGenerator
-    pwd = generator.generate_password(pwdLength=pass_length, pwdName=pass_name)
+    pwd = pwdgen.generate_password(pwdLength=pass_length)
     response = {"generated_password": pwd}
     return (response)
 
